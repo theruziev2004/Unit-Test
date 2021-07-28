4a5341270209b72670717cf84f7da2c807152908
theruziev2004 <72446044+theruziev2004@users.noreply.github.com>
2021-07-28 16:35:24 +0500

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -4,20 +4,35 @@ import "testing"
 
 func TestArithmeticMean(t *testing.T) {
 	// Настройка тестовых данных
-	numbers := []int {1, 2, 3, 4, 5}
-	expected := 3.0
+	testTable := []struct{
+		numbers []int
+		expected float64
+	}{
+	{
+		numbers: []int {1, 2, 3, 4, 5},
+		expected: 3.0,
+	},
+	{
+		numbers: []int {6, 7, 8, 9, 10},
+		expected: 8.0,
+	},
+	{
+		numbers: []int {54, 45, 85, 48, 78},
+		expected: 62.0,
+	},
+	}
+
 
 	// Вызов тестируемого кода
-	result := ArithmeticMean(numbers)
+	for _, testCase := range testTable{
+		result := ArithmeticMean(testCase.numbers)
 
-	// Проверка возвращаемых результатов
-	if result != expected {
-		t.Errorf("Incorrect result")
+		// Проверка возвращаемых результатов
+		if result != testCase.expected {
+			t.Errorf("Incorrect result")
+		}
 	}
+
+
+
 }
-
-
-
-
-
-
